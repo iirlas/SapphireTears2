250ceb5d4f68defc11ce0504373ce7b331cc4209
iirlas <iirlas@yahoo.com>
2017-11-29 20:25:29 -0600

    Revised Source\Sapphire2\Sapphire2GameMode.cpp, Initializes the PlayerControllerClass to AMyPlayerController

--- Source/Sapphire2/Sapphire2GameMode.cpp
+++ Source/Sapphire2/Sapphire2GameMode.cpp
@@ -4,6 +4,7 @@
 #include "Sapphire2HUD.h"
 #include "Sapphire2Character.h"
 #include "UObject/ConstructorHelpers.h"
+#include "MyPlayerController.h"
 
 ASapphire2GameMode::ASapphire2GameMode()
 	: Super()
@@ -11,6 +12,8 @@ ASapphire2GameMode::ASapphire2GameMode()
 	// set default pawn class to our Blueprinted character
 	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/FirstPersonCPP/Blueprints/FirstPersonCharacter"));
 	DefaultPawnClass = PlayerPawnClassFinder.Class;
+
+	PlayerControllerClass = AMyPlayerController::StaticClass();
 
 	// use our custom HUD class
 	HUDClass = ASapphire2HUD::StaticClass();
